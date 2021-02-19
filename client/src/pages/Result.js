4e3fd8d162e42cf39e6701f36294d36a2cbb4e4f
SankhaChak <anonproductionspro@gmail.com>
2021-02-20 00:40:46 +0530

    style: 💅 basic styling

--- client/src/pages/Result.js
+++ client/src/pages/Result.js
@@ -1,5 +1,26 @@
 const Result = () => {
-  return <div>Result</div>;
+  return (
+    <section className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="text-center mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-white mb-4">
+            Payment Accepted!
+          </h1>
+          <p className="leading-relaxed xl:w-2/4 lg:w-3/4 text-xl mx-auto">
+            Below is your order summary. The items will be shipped to you within
+            the next week.
+          </p>
+          <br />
+          <h2 className="text-xl text-indigo-400 tracking-widest font-medium title-font mb-1">
+            Order Total: "total"
+          </h2>
+          <h2 className="text-xl text-indigo-400 tracking-widest font-medium title-font mb-1">
+            Email: {"data.customer_details.email"}
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Result;
